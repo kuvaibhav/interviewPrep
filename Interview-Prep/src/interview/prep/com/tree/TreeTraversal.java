3457cc3d428a5fdd97e68924dbc4c8826ebc9ce2
Kumar Vaibhav <kumar.vaibhav002@gmail.com>
2021-09-03 22:59:56 -0400

    Projetc setup

---
+++ Interview-Prep/src/interview/prep/com/tree/TreeTraversal.java
@@ -0,0 +1,13 @@
+package interview.prep.com.tree;
+
+public class TreeTraversal {
+	Node root;
+
+	public static void main(String[] args) {
+		Node root = new Node(7);
+		root.right = new Node(8);
+		root.left = new Node(6);
+
+	}
+
+}
